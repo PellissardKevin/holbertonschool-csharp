2c8a79fc8e29c46bdbf87159d346ece74e397985
Kevin.P <kevin.pellissard@gmail.com>
2024-04-08 12:59:41 +0200

    update with new line

--- csharp-hello_world/3-writeline/obj/Debug/net8.0/3-writeline.AssemblyInfo.cs
+++ csharp-hello_world/3-writeline/obj/Debug/net8.0/3-writeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58da02e81b5cfd70e9c0ea93053cadc1b9d3dd40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a15bb41ae12ac421774f6cf1795b063ff3245c")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
