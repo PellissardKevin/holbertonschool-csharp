9ac865bc8ee50a082c086aa37c6fb1e53fe73dcb
Kevin.P <kevin.pellissard@gmail.com>
2024-04-08 22:34:26 +0200

    Add task 6 print string 3 times and print 9 char

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		
+		string repeatedStr = string.Concat(Enumerable.Repeat(str, 3));
+		Console.Write(repeatedStr);
+
+		string firstNineChars = str.Substring(0, Math.Min(str.Length, 9));
+		Console.WriteLine(firstNineChars);
+	}
+}
