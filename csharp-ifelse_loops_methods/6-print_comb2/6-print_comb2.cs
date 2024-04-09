cfea166d2b846407fdbd0241f32615727a3baa08
Kevin.P <kevin.pellissard@gmail.com>
2024-04-09 13:41:05 +0200

    Task 6 print the smallest combination of two digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int printed = 0;
+            for (int i = 0; i <= 8; i++)
+                for (int j = i + 1; j <= 9; j++)
+                {
+                    if (printed > 0)
+                    {
+                        Console.Write(", ");
+                    }
+                    Console.Write($"{i}{j}");
+                    printed++;
+                }
+
+                Console.WriteLine();
+        }
+    }
+}
