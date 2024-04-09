d16b2dea8870680818791f5c2f953cd6a3a1e77a
Kevin.P <kevin.pellissard@gmail.com>
2024-04-09 13:28:13 +0200

    Task 3 print alphabet without e and q, task 4 print in hexa; task 5 print combination

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+                if (i != 'e' && i != 'q')
+                    Console.Write(i);
+        }
+    }
+}
