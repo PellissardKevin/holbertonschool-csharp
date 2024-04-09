85bba95c9ee85bf8ad2f534e85a88fa471294222
Kevin.P <kevin.pellissard@gmail.com>
2024-04-09 11:59:14 +0200

    Task 1 Last digit of number

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % (10);
+
+        Console.Write("The last digit of {0}", number);
+        if (lastDigit > 5)
+            Console.WriteLine(" is {0} and is greater than 5", lastDigit);
+        else if (lastDigit < 6 && lastDigit != 0)
+            Console.WriteLine(" is {0} and is less than 6 and not 0", lastDigit);
+        else if (lastDigit == 0)
+            Console.WriteLine(" is {0} and is 0", lastDigit);
+    }
+}
