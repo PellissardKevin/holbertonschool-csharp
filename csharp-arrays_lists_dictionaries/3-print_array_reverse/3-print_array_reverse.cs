904cfdb36660236e1a0ecd4c5dc4bb6876ff8cd7
Kevin.P <kevin.pellissard@gmail.com>
2024-04-15 12:37:49 +0200

    Task 2 replace element, task 3 print reverse array and task 4 print list

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null && array.Length != 0)
+        {
+            int[] result = new int[array.Length];
+            for (int i = array.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                result[j] = array[i];
+            }
+            Console.WriteLine(string.Join(" ", result)); ;
+        }
+        else
+            Console.WriteLine();
+
+    }
+}
