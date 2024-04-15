812a7c30c2bba97958a64b70379edacf5512f404
Kevin.P <kevin.pellissard@gmail.com>
2024-04-15 11:13:38 +0200

    Task 0  create and print an array of a given size

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            int[] result = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = i;
+            }
+            Console.WriteLine(string.Join(" ", result)); // Print the array elements separated by space
+            return result; // Return the array
+        }
+    }
+}
+
