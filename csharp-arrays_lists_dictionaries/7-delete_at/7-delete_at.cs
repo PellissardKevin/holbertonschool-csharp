e14e6260b883e3029f3f573ca396f628ee3936fe
Kevin.P <kevin.pellissard@gmail.com>
2024-04-16 19:59:46 +0200

    Update for trying something

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -11,14 +11,14 @@ class List
             return myList;
         }
 
-        for (int i = index; i < myList.Count; i++)
-        {
-            if (i + 1 < myList.Count)
-            {
-                myList[i] = myList[i + 1];
-            }
-        }
-        myList.RemoveRange(myList.Count - 1, 1);
+        // for (int i = index; i < myList.Count; i++)
+        // {
+        //     if (i + 1 < myList.Count)
+        //     {
+        //         myList[i] = myList[i + 1];
+        //     }
+        // }
+        myList.RemoveRange(index, 1);
         return myList;
     }
 }
