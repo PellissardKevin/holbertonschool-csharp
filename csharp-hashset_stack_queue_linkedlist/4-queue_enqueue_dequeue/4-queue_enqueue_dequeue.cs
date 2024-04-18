ab05cea85b5ed5e48c88e57c7666179f57e41516
Kevin.P <kevin.pellissard@gmail.com>
2024-04-18 11:22:03 +0200

    Task 4 same than 3 but with queue

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count > 0)
+        {
+            Console.WriteLine($"Top item: {aQueue.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        bool containsSearch = aQueue.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {containsSearch}");
+
+
+        if (containsSearch)
+        {
+            while (true)
+            {
+                var item = aQueue.Peek();
+                aQueue.Dequeue();
+                if (item == search)
+                {
+                    break;
+                }
+            }
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
+
