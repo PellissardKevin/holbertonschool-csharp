34488d0aa690dfc7fb2387b0abccdf0a3ea0ddd9
Kevin.P <kevin.pellissard@gmail.com>
2024-05-06 12:02:51 +0200

    Add task 0 to 3 in c# exercice namespace

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Zombie class</summary>
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
