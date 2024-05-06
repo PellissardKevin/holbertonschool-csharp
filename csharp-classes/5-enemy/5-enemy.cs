6ca5b34f531b1a05689633e9ca6cc3a4294a3633
Kevin.P <kevin.pellissard@gmail.com>
2024-05-06 12:27:31 +0200

    dd task 5 return ToString method

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Zombie class</summary>
+    class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            health = value;
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+        public string Name
+        {
+            set {
+                name = value;
+            }
+            get {
+                return name;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
