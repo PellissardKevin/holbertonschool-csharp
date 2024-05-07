5e07ad850ef3209beb0506e022578ee16dca59f3
Kevin.P <kevin.pellissard@gmail.com>
2024-05-07 10:31:18 +0200

    Update commentary for task 0 TDD in C#

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,15 +2,9 @@
 
 namespace MyMath
 {
-    /// <summary>
-    /// Provides a collection of math operations
-    /// </summary>
-    class Operations
+
+    public class Operations
     {
-        /// <summary>
-        /// Adds two integers together
-        /// </summary>
-        /// <returns>The sum of two integers</returns>
         public static int Add(int a, int b)
         {
             return a + b;
