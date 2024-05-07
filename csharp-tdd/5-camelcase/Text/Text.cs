86e8ddbdc5718c87fc6f2faee8d2a4d729034c97
Kevin.P <kevin.pellissard@gmail.com>
2024-05-07 20:38:39 +0200

    Add task 5 tdd c# exercise for camelCase string

---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static int CamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            int nbWord = 1;
+
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    if (s.IndexOf(c) != 0)
+                    {
+                        nbWord++;
+                    }
+                }
+            }
+
+            return nbWord;
+        }
+    }
+}
