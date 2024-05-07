ed3a87a210a5a81068a718efb2b3027d1b5055ce
Kevin.P <kevin.pellissard@gmail.com>
2024-05-07 12:56:44 +0200

    Task 4 TDD Exercices for Uniques

---
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static int UniqueChar(string s)
+        {
+            if (s == null)
+            {
+                return -1;
+            }
+
+            if (s.Length == 1)
+            {
+                return 0;
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                // If current character does not appear elsewhere in string
+                if (s.Substring(i + 1).Contains(s[i].ToString()) == false &&
+                    s.Substring(0, i).Contains(s[i].ToString()) == false)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
