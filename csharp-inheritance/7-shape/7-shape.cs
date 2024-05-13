ed38200ede1c4b677c8bf3972c7a52ace2f62d06
Kevin.P <kevin.pellissard@gmail.com>
2024-05-13 13:20:57 +0200

    update task 6 and 7 with comments

--- csharp-inheritance/7-shape/7-shape.cs
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -16,37 +16,58 @@ public class Shape
 }
 
 /// <summary>
-/// Represents a rectangle/>.
+/// Represents a rectangle shape that inherits from <see cref="Shape"/>.
 /// </summary>
 public class Rectangle : Shape
 {
     private int width;
     private int height;
 
-
+    /// <summary>
+    /// Gets or sets the width of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when the provided width is negative.</exception>
     public int Width
     {
         get => width;
         set
         {
             if (value < 0)
-            {
                 throw new ArgumentException("Width must be greater than or equal to 0.");
-            }
             width = value;
         }
     }
 
+    /// <summary>
+    /// Gets or sets the height of the rectangle.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when the provided height is negative.</exception>
     public int Height
     {
         get => height;
         set
         {
             if (value < 0)
-            {
                 throw new ArgumentException("Height must be greater than or equal to 0.");
-            }
             height = value;
         }
+    }
+
+    /// <summary>
+    /// Calculates the area of the rectangle.
+    /// </summary>
+    /// <returns>The area of the rectangle.</returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    /// <summary>
+    /// Returns a string representation of the rectangle.
+    /// </summary>
+    /// <returns>A string representation of the rectangle.</returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
     }
 }
