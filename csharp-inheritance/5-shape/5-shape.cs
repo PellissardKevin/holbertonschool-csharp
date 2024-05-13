b3e152e7dcb14799bdfb8e1235f754ec3af4fb2b
Kevin.P <kevin.pellissard@gmail.com>
2024-05-13 13:11:14 +0200

    TAsk 5 with exception

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
