e927aa7d6296be76828e94de35b7cfac21459b3d
Kevin.P <kevin.pellissard@gmail.com>
2024-05-13 12:57:02 +0200

    Add task 2 and 3

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (baseType.IsAssignableFrom(derivedType) && derivedType != baseType)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+public class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type objectType = myObj.GetType();
+
+        // Get properties
+        PropertyInfo[] properties = objectType.GetProperties();
+        Console.WriteLine("Int32 Properties:");
+        foreach (PropertyInfo property in properties)
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        // Get methods
+        MethodInfo[] methods = objectType.GetMethods();
+        Console.WriteLine("Int32 Methods:");
+        foreach (MethodInfo method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
