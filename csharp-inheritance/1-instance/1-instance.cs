19f43dc1b443a56f84f83948ed7a1792f3ff1af1
Kevin.P <kevin.pellissard@gmail.com>
2024-05-13 11:09:22 +0200

    Task 1 type of an instance or inherit

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return typeof(Array).IsInstanceOfType(obj);
+    }
+}
+
