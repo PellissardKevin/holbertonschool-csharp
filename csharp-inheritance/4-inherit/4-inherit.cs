9de3ffeccdc8554be39cfca7e4bfb39621ca9d84
Kevin.P <kevin.pellissard@gmail.com>
2024-05-13 13:09:40 +0200

    Update task 4 with comment

--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,11 +1,15 @@
-﻿using System;
-
-
+﻿/// <summary>
+/// Represents an empty class for animals.
+/// </summary>
 public class Animal
 {
+    // This class is intentionally left empty
 }
 
+/// <summary>
+/// Represents an empty class for dogs, inheriting from <see cref="Animal"/>.
+/// </summary>
 public class Dog : Animal
 {
-
+    // This class is intentionally left empty
 }
