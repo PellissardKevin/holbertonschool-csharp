528e13144b2f4f8534ab91e96f9bf62726b546f3
Kevin.P <kevin.pellissard@gmail.com>
2024-05-14 09:51:05 +0200

    Update task 7

--- csharp-inheritance/7-shape/7-shape.cs
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -1,32 +1,20 @@
 ﻿using System;
 
-/// <summary>
-/// Represents a basic shape with an area calculation method.
-/// </summary>
-public class Shape
+
+class Shape
 {
-    /// <summary>
-    /// Calculates the area of the shape.
-    /// </summary>
-    /// <returns>The area of the shape.</returns>
+
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
 
-/// <summary>
-/// Represents a rectangle shape that inherits from <see cref="Shape"/>.
-/// </summary>
-public class Rectangle : Shape
+class Rectangle : Shape
 {
     private int width;
     private int height;
 
-    /// <summary>
-    /// Gets or sets the width of the rectangle.
-    /// </summary>
-    /// <exception cref="ArgumentException">Thrown when the provided width is negative.</exception>
     public int Width
     {
         get => width;
@@ -38,10 +26,7 @@ public class Rectangle : Shape
         }
     }
 
-    /// <summary>
-    /// Gets or sets the height of the rectangle.
-    /// </summary>
-    /// <exception cref="ArgumentException">Thrown when the provided height is negative.</exception>
+
     public int Height
     {
         get => height;
@@ -53,19 +38,12 @@ public class Rectangle : Shape
         }
     }
 
-    /// <summary>
-    /// Calculates the area of the rectangle.
-    /// </summary>
-    /// <returns>The area of the rectangle.</returns>
+
     public new int Area()
     {
         return Width * Height;
     }
 
-    /// <summary>
-    /// Returns a string representation of the rectangle.
-    /// </summary>
-    /// <returns>A string representation of the rectangle.</returns>
     public override string ToString()
     {
         return $"[Rectangle] {width} / {height}";
