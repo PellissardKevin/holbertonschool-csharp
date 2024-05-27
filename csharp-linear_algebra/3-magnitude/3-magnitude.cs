411b7d7d0a7a7360c1d1efce870ef4cff9d7306e
Kevin.P <kevin.pellissard@gmail.com>
2024-05-27 11:59:35 +0200

    Task to calculate length of a 2D or 3D vector

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int dimensions = vector.Length;
+
+        if (dimensions != 2 || dimensions != 3)
+        {
+            return (-1);
+        }
+
+        return dimensions;
+    }
+}
+
