93977fc2d6f6ba4ea377e2500200a951ec0368e1
Kevin.P <kevin.pellissard@gmail.com>
2024-05-27 12:55:55 +0200

    Task 6 Vector addition, for 2 vecotr 2D or 3D

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class VectorMath
+{
+
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if ((vector1.Length == 2 || vector1.Length == 3) && (vector2.Length == 2 || vector2.Length == 3) && vector1.Length == vector2.Length)
+        {
+             Vector vectorResult = new Vector();
+
+             vectorResult = Vector.Add(vector1, vector2);
+
+             return vectorResult;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
