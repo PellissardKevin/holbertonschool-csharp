60c35d2d9ede1a6525331fd79b76a0020666f6c5
Kevin.P <kevin.pellissard@gmail.com>
2024-05-27 13:26:40 +0200

    Task 9 scale a vector and return the neww vector

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 2 || vector.Length == 3)
+        {
+            new double[] MulVector = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] *= scalar;
+            }
+
+            return vector;
+        }
+        else
+        {
+            return new double[] {-1};
+        }
+    }
+}
+
