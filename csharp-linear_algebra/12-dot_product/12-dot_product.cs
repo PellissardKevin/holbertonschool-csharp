4a3fb86683fd8b2d506270a5c2b93492d5551c57
Kevin.P <kevin.pellissard@gmail.com>
2024-05-27 13:59:00 +0200

    Update task 12 and add task 14 for adding two matrices

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -2,7 +2,7 @@
 
 public class VectorMath
 {
-    public static double? DotProduct(double[] vector1, double[] vector2)
+    public static double DotProduct(double[] vector1, double[] vector2)
     {
         if (vector1.Length == vector2.Length && (vector1.Length == 2 || vector1.Length == 3))
         {
@@ -11,7 +11,7 @@ public class VectorMath
             // Calculate the dot product
             for (int i = 0; i < vector1.Length; i++)
                 vector1[i] *= vector2[i];
-    
+
             // Return sum of products
             return vector1.Sum();
         }
---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        // Get the dimensions of the matrices
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        // Check if the matrices have the same dimensions
+        if ((rows1 == rows2 && cols1 == cols2) &&
+            ((rows1 == 2 && cols1 == 2) || (rows1 == 3 && cols1 == 3)))
+        {
+            for (int y = 0; y < matrix1.GetLength(0); y++)
+            {
+                for (int x = 0; x < matrix2.GetLength(1); x++)
+                {
+                    // Add together the corresponding values
+                    matrix1[y, x] += matrix2[y, x];
+                }
+            }
+            return matrix1;
+        }
+
+        return new double[,] { { -1 } };
+    }
+}
