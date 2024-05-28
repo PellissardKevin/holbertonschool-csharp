2d9a4f01b43c8235b16f2d73e89f8a0bd8e2f915
Kevin.P <kevin.pellissard@gmail.com>
2024-05-28 19:32:41 +0200

    task 18 multiply two matrix

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+/// <summary>
+/// Provides a suite of linear algebra helper methods.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Adds two 2D or 3D matrices.
+    /// </summary>
+    /// <param name="matrix1">the first matrix.</param>
+    /// <param name="matrix2">yhe second matrix</param>
+    /// <returns>
+    /// The result of two matrix multiply, or -1 if matrix is of invalid size.
+    /// </returns>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        //Check if matrices can be multiplied
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return new double[,] { { -1 } };
+
+        double[,] resultMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                resultMatrix[i, j] = 0;
+
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return resultMatrix;
+    }
+}
