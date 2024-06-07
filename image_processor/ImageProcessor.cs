a40832993a46521970122f8fa5d38c022620ab15
Kevin.P <kevin.pellissard@gmail.com>
2024-06-07 15:10:25 +0200

    Add task to invert image. ImageProcessor in c#

---
+++ image_processor/ImageProcessor.cs
@@ -0,0 +1,49 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        var tasks = new Task[filenames.Length];
+
+        for (int i = 0; i < filenames.Length; i++)
+        {
+            string filename = filenames[i];
+            tasks[i] = Task.Run(() => InverseImage(filename));
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    private static void InverseImage(string filename)
+    {
+        try
+        {
+            using (Image<Rgba32> image = Image.Load<Rgba32>(filename))
+            {
+                image.Mutate(ctx => ctx.Invert());
+
+                string newFilename = GetNewFilename(filename);
+                image.Save(newFilename);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing file {filename}: {ex.Message}");
+        }
+    }
+
+    private static string GetNewFilename(string originalFilename)
+    {
+        string directory = Path.GetDirectoryName(originalFilename);
+        string filenameWithoutExtension = Path.GetFileNameWithoutExtension(originalFilename);
+        string extension = Path.GetExtension(originalFilename);
+
+        return Path.Combine(directory, $"{filenameWithoutExtension}_inverse{extension}");
+    }
+}
---
+++ image_processor/README.md
@@ -0,0 +1,23 @@
+What you should learn
+
+At the end of this project you are expected to be able to explain to anyone, without the help of Google:
+
+    What is parallel programming?
+    What is concurrency?
+    What is threading?
+    When is it appropriate to use multiple threads? When is it not?
+    How do you create, start, pause, resume, and abort threads?
+    What are threading best practices?
+    What is meant by ‘thread-safe’?
+    What is the difference between processes and threads?
+    What kind of applications benefit from multithreading? What applications don’t?
+
+Requirements for C# projects
+
+    Allowed editors: Visual Studio Code
+    All files will be compiled on Ubuntu 14.04 LTS using dotnet
+    A README.md file, at the root of the folder of the project, is mandatory
+    You do not need to push your obj/ or bin/ folders
+    In the following examples, the main.cs files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.cs files at compilation. Our main.cs files might be different from the one shown in the examples
+    All your public classes and their members should have XML documentation tags
+    All your private classes and members should be documented but without XML documentation tags
