d298addc34fe994b4fc4d0f08052a42908036890
Kevin.P <kevin.pellissard@gmail.com>
2024-06-13 15:23:08 +0200

    Try to increase efficiency with parallele.for

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Threading.Tasks;
+using System.Numerics;
 
 class ImageProcessor
 {
@@ -22,24 +22,32 @@ class ImageProcessor
                 BitmapData bmpData = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
 
                 int stride = bmpData.Stride;
-                int bytes = stride * height;
-                byte[] pixelBuffer = new byte[bytes];
+                int bytesPerPixel = Image.GetPixelFormatSize(bmpData.PixelFormat) / 8;
+                int byteCount = stride * height;
+                byte[] pixelBuffer = new byte[byteCount];
 
-                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, bytes);
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
 
-                // Iterate through the pixels sequentially
-                for (int i = 0; i < pixelBuffer.Length; i += 4)
+                int vectorSize = Vector<byte>.Count;
+                byte[] maxValues = new byte[vectorSize];
+                for (int j = 0; j < vectorSize; j++)
+                    maxValues[j] = 255;
+                Vector<byte> maxVector = new Vector<byte>(maxValues);
+
+                Parallel.For(0, pixelBuffer.Length / vectorSize, i =>
                 {
-                    pixelBuffer[i] ^= 0xFF;        // Blue
-                    pixelBuffer[i + 1] ^= 0xFF;    // Green
-                    pixelBuffer[i + 2] ^= 0xFF;    // Red
-                }
+                    int offset = i * vectorSize;
+                    if (offset + vectorSize > pixelBuffer.Length) return;
 
-                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, bytes);
+                    Vector<byte> pixelVector = new Vector<byte>(pixelBuffer, offset);
+                    Vector<byte> resultVector = maxVector - pixelVector;
+                    resultVector.CopyTo(pixelBuffer, offset);
+                });
+
+                System.Runtime.InteropServices.Marshal.Copy(pixelBuffer, 0, bmpData.Scan0, pixelBuffer.Length);
 
                 image.UnlockBits(bmpData);
 
-                // Generate new filename
                 string[] nameSplit = imagePath.Split(new char[] { '/', '.' });
                 string newFilename = $"{nameSplit[nameSplit.Length - 2]}_inverse.{nameSplit[nameSplit.Length - 1]}";
 
