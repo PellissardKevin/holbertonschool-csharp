85d5f9cf7d3023f171ab045d5e3c0a4fea059f27
Kevin.P <kevin.pellissard@gmail.com>
2024-07-22 16:04:01 +0200

    add documentation xml for every project

--- csharp-generics/2-dequeue/queue.cs
+++ csharp-generics/2-dequeue/queue.cs
@@ -1,34 +1,66 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a generic queue data structure.
+/// </summary>
+/// <typeparam name="T">Specifies the type of elements in the queue.</typeparam>
 class Queue<T>
 {
-
+    /// <summary>
+    /// Gets or sets the node at the front of the queue.
+    /// </summary>
     public Node head { get; set; } = null;
-    public Node tail { get; set; } = null;
-    int count { get; set; } = 0;
 
+    /// <summary>
+    /// Gets or sets the node at the end of the queue.
+    /// </summary>
+    public Node tail { get; set; } = null;
+
+    /// <summary>
+    /// Gets or sets the number of elements in the queue.
+    /// </summary>
+    private int count { get; set; } = 0;
+
+    /// <summary>
+    /// Returns the type of elements stored in the queue.
+    /// </summary>
+    /// <returns>The type of elements stored in the queue.</returns>
     public Type CheckType()
     {
         return typeof(T);
     }
 
+    /// <summary>
+    /// Represents a node in the queue.
+    /// </summary>
     public class Node
     {
-
+        /// <summary>
+        /// Gets or sets the value stored in the node.
+        /// </summary>
         public T value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next node in the queue.
+        /// </summary>
         public Node next { get; set; } = null;
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class with the specified value.
+        /// </summary>
+        /// <param name="_value">The value to store in the node.</param>
         public Node(T _value)
         {
             value = _value;
         }
-
     }
 
+    /// <summary>
+    /// Adds an element to the end of the queue.
+    /// </summary>
+    /// <param name="_value">The value to add to the queue.</param>
     public void Enqueue(T _value)
     {
-
         Node newNode = new Node(_value);
 
         // If head does not exist, set new node as head and tail.
@@ -46,40 +78,42 @@ class Queue<T>
         count++;
     }
 
-
+    /// <summary>
+    /// Removes and returns the element at the front of the queue.
+    /// </summary>
+    /// <returns>The value of the element at the front of the queue, or the default value of T if the queue is empty.</returns>
     public T Dequeue()
     {
-
         if (head != null)
         {
-
             T value = head.value;
 
-            // If node being removed is only node...
+            // If node being removed is the only node...
             if (head == tail)
             {
                 tail = null;
             }
 
-            // Move head forward to reference next node.
+            // Move head forward to reference the next node.
             head = head.next;
 
             // Decrement count of nodes in queue.
             count--;
 
             return value;
-
         }
         else
         {
-
-            // If queue is empty...
+            // If the queue is empty...
             Console.WriteLine("Queue is empty");
             return default(T);
-
         }
     }
 
+    /// <summary>
+    /// Gets the number of elements in the queue.
+    /// </summary>
+    /// <returns>The number of elements in the queue.</returns>
     public int Count()
     {
         return count;
