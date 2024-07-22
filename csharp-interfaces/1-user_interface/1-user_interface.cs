42bca800965d2bddfd90b5a5fe31d55fc40f055d
Kevin.P <kevin.pellissard@gmail.com>
2024-07-22 16:35:59 +0200

    Add task 0 and 1 for project interfaces

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int Durability { get; set; }
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool IsCollected { get; set; }
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    // Implement the properties for IBreakable and ICollectable
+    public int Durability { get; set; }
+    public bool IsCollected { get; set; }
+
+    // Implement the methods for IInteractive, IBreakable, and ICollectable
+    public void Interact()
+    {
+        // Method implementation here
+    }
+
+    public void Break()
+    {
+        // Method implementation here
+    }
+
+    public void Collect()
+    {
+        // Method implementation here
+    }
+}
