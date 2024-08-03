5c53b20bd3a87e4524be5a5bd33233e9379bdd22
Kevin.P <kevin.pellissard@gmail.com>
2024-08-03 18:03:53 +0200

    Update unit test and check if the app work well for reviewing.

---
+++ csharp-text_based_interface/InventoryManagement.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using InventoryLibrary;
+
+namespace InventoryManagement.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
