cef79cddf7536beb0d19509f8e1d77aa1f69804a
Kevin.P <kevin.pellissard@gmail.com>
2024-08-05 13:44:13 +0200

    Update unit test

--- csharp-text_based_interface/InventoryManagement.Tests/UnitTest1.cs
+++ csharp-text_based_interface/InventoryManagement.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
+using System;
 using InventoryLibrary;
+using System.IO;
 
 namespace InventoryManagement.Tests
 {
@@ -8,10 +10,23 @@ namespace InventoryManagement.Tests
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("seting up");
+            File.Copy("../../../../storage/inventory_manager.json", "../../../../storage/backup_inventory_manager.json", overwrite: true);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("tearing down");
+            File.Copy("../../../../storage/backup_inventory_manager.json", "../../../../storage/inventory_manager.json", overwrite: true);
         }
 
         [Test]
         public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Test2()
         {
             Assert.Pass();
         }
