653271973174fa78b26a56e84b54c89d8735bbb2
Kevin.P <kevin.pellissard@gmail.com>
2024-08-06 14:34:11 +0200

    Update test unit

--- csharp-text_based_interface/InventoryManagement.Tests/UnitTest1.cs
+++ csharp-text_based_interface/InventoryManagement.Tests/UnitTest1.cs
@@ -7,19 +7,6 @@ namespace InventoryManagement.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-            Console.WriteLine("seting up");
-            File.Copy("../../../../storage/inventory_manager.json", "../../../../storage/backup_inventory_manager.json", overwrite: true);
-        }
-        [TearDown]
-        public void TearDown()
-        {
-            Console.WriteLine("tearing down");
-            File.Copy("../../../../storage/backup_inventory_manager.json", "../../../../storage/inventory_manager.json", overwrite: true);
-        }
-
         [Test]
         public void Test1()
         {
